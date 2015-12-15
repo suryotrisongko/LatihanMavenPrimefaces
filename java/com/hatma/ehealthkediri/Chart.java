54112aa38bb9c5c1f1ff0c55aa8a83ce49d81d33
suryotrisongko <suryotrisongko@gmail.com>
2015-12-15 14:05:20 +0700

    hatma: update kevin

--- java/com/hatma/ehealthkediri/Chart.java
+++ java/com/hatma/ehealthkediri/Chart.java
@@ -11,17 +11,19 @@ import org.primefaces.event.ItemSelectEvent;
  
 import org.primefaces.model.chart.Axis;
 import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
 import org.primefaces.model.chart.HorizontalBarChartModel;
 import org.primefaces.model.chart.ChartSeries;
  
 @ManagedBean
 public class Chart implements Serializable {
  
+    private BarChartModel barModel;
     private HorizontalBarChartModel horizontalBarModel;
     private ChartSeries laki, prm;
     private String jeniskelamin = "Perempuan";
  
-     public String getjeniskelamin() {
+    public String getjeniskelamin() {
         return jeniskelamin;
     }
 
@@ -32,18 +34,35 @@ public class Chart implements Serializable {
     public void init() {
         createHorizontalBarModels();
     }
- 
-     
+    
     public HorizontalBarChartModel getHorizontalBarModel() {
         return horizontalBarModel;
     }
-   public void refreshChart3() {
+    
+    public BarChartModel getBarModel() {
+        return barModel;
+    }
+    
+    public void refreshChart3() {
+        createHorizontalBarModel();
+    }
+    
+    public void refreshChart2() {
         createHorizontalBarModel();
     }
  
+    private void createHorizontalBarModels() {
+        createHorizontalBarModel();
+        createBarModel();
+    }
+    
     private HorizontalBarChartModel createHorizontalBarModel() {
         horizontalBarModel = new HorizontalBarChartModel();
  
+        horizontalBarModel.setTitle("10 Penderita Berdasarkan Jenis Kelamin");
+        horizontalBarModel.setLegendPosition("e");
+        horizontalBarModel.setStacked(true);
+        
         ChartSeries laki = new ChartSeries();
         laki.setLabel("Laki - laki");
         laki.set("pilek", 79);
@@ -70,15 +89,8 @@ public class Chart implements Serializable {
         prm.set("katarak", 99);
         prm.set("panu", 84);
        
-        
-        
         horizontalBarModel.addSeries(laki);
         horizontalBarModel.addSeries(prm);
-        
-         
-        horizontalBarModel.setTitle("10 Penderita Berdasarkan Jenis Kelamin");
-        horizontalBarModel.setLegendPosition("e");
-        horizontalBarModel.setStacked(true);
          
         Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
         xAxis.setLabel("Jenis Kelamin");
@@ -91,9 +103,52 @@ public class Chart implements Serializable {
         return horizontalBarModel;
     }
      
-    private void createHorizontalBarModels() {
-        createHorizontalBarModel();
+    private void createBarModel() {
+        barModel = new BarChartModel();
+
+        barModel.setTitle("10 Penderita Berdasarkan Jenis Kelamin");
+        barModel.setLegendPosition("ne");
+        barModel.setStacked(true);
+        barModel.setAnimate(true);
+        
+        ChartSeries laki = new ChartSeries();
+        laki.setLabel("Laki - laki");
+        laki.set("Pilek", 79);
+        laki.set("Flu", 101);
+        laki.set("Batuk", 81);
+        laki.set("Berdahak", 89);
+        laki.set("Meriang", 74);
+        laki.set("Masuk angin", 102);
+        laki.set("Panas dalam", 78);
+        laki.set("Kejang", 89);
+        laki.set("Katarak", 88);
+        laki.set("Panu", 91);
+ 
+        ChartSeries prm = new ChartSeries();
+        prm.setLabel("Perempuan");
+        prm.set("Pilek", 113);
+        prm.set("Flu", 99);
+        prm.set("Batuk", 106);
+        prm.set("Berdahak", 83);
+        prm.set("Meriang", 86);
+        prm.set("Masuk angin", 87);
+        prm.set("Panas dalam", 94);
+        prm.set("Kejang", 75);
+        prm.set("Katarak", 99);
+        prm.set("Panu", 84);
+       
+        barModel.addSeries(laki);
+        barModel.addSeries(prm);
+        
+        Axis xAxis = barModel.getAxis(AxisType.X);
+        xAxis.setLabel("Penyakit");
+                 
+        Axis yAxis = barModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Jenis Kelamin");
+        yAxis.setMin(0);
+        yAxis.setMax(300);
     }
+    
      public void itemSelectHorizontalBarModel(ItemSelectEvent event) {
         FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Item selected",
                 "Item Index: " + event.getItemIndex() + ", Series Index:" + event.getSeriesIndex());
--- java/com/hatma/ehealthkediri/ChartView.java
+++ java/com/hatma/ehealthkediri/ChartView.java
@@ -13,6 +13,7 @@ import org.primefaces.model.chart.ChartSeries;
 @ManagedBean
 public class ChartView implements Serializable {
  
+    private BarChartModel barModel;
     private HorizontalBarChartModel horizontalBarModel;
  
     @PostConstruct
@@ -20,12 +21,19 @@ public class ChartView implements Serializable {
         createBarModels();
     }
  
+    private void createBarModels() {
+        createHorizontalBarModel();
+        createBarModel();
+    }
      
     public HorizontalBarChartModel getHorizontalBarModel() {
         return horizontalBarModel;
     }
    
- 
+    public BarChartModel getBarModel() {
+        return barModel;
+    }
+    
     private HorizontalBarChartModel createHorizontalBarModel() {
         horizontalBarModel = new HorizontalBarChartModel();
  
@@ -130,8 +138,107 @@ public class ChartView implements Serializable {
         return horizontalBarModel;
     }
      
-    private void createBarModels() {
-        createHorizontalBarModel();
+    private void createBarModel() {
+        barModel = new BarChartModel();
+
+        barModel.setTitle("10 Penderita Berdasarkan Usia");
+        barModel.setLegendPosition("ne");
+        barModel.setStacked(true);
+        barModel.setAnimate(true);
+        
+        ChartSeries bayi = new ChartSeries();
+        bayi.setLabel("bayi");
+        bayi.set("pilek", 64);
+        bayi.set("flu", 61);
+        bayi.set("batuk", 53);
+        bayi.set("berdahak", 65);
+        bayi.set("meriang", 43);
+        bayi.set("masuk angin", 60);
+        bayi.set("panas dalam", 46);
+        bayi.set("kejang", 62);
+        bayi.set("katarak", 53);
+        bayi.set("panu", 69);
+ 
+        ChartSeries balita = new ChartSeries();
+        balita.setLabel("balita");
+        balita.set("pilek", 64);
+        balita.set("flu", 61);
+        balita.set("batuk", 53);
+        balita.set("berdahak", 65);
+        balita.set("meriang", 43);
+        balita.set("masuk angin", 60);
+        balita.set("panas dalam", 46);
+        balita.set("kejang", 62);
+        balita.set("katarak", 53);
+        balita.set("panu", 69);
+        
+        ChartSeries anak = new ChartSeries();
+        anak.setLabel("anak");
+        anak.set("pilek", 38);
+        anak.set("flu", 52);
+        anak.set("batuk", 39);
+        anak.set("berdahak", 33);
+        anak.set("meriang", 27);
+        anak.set("masuk angin", 46);
+        anak.set("panas dalam", 35);
+        anak.set("kejang", 24);
+        anak.set("katarak", 41);
+        anak.set("panu", 42);
+        
+        ChartSeries rema = new ChartSeries();
+        rema.setLabel("remaja");
+        rema.set("pilek", 26);
+        rema.set("flu", 30);
+        rema.set("batuk", 30);
+        rema.set("berdahak", 21);
+        rema.set("meriang", 28);
+        rema.set("masuk angin", 37);
+        rema.set("panas dalam", 23);
+        rema.set("kejang", 14);
+        rema.set("katarak", 37);
+        rema.set("panu", 23);
+        
+        ChartSeries dws = new ChartSeries();
+        dws.setLabel("dewasa");
+        dws.set("pilek", 14);
+        dws.set("flu", 22);
+        dws.set("batuk", 23);
+        dws.set("berdahak", 17);
+        dws.set("meriang", 21);
+        dws.set("masuk angin", 27);
+        dws.set("panas dalam", 11);
+        dws.set("kejang", 14);
+        dws.set("katarak", 25);
+        dws.set("panu", 12);
+        
+        ChartSeries  tua= new ChartSeries();
+        tua.setLabel("55");
+        tua.set("pilek", 16);
+        tua.set("flu", 19);
+        tua.set("batuk", 26);
+        tua.set("berdahak", 12);
+        tua.set("meriang", 25);
+        tua.set("masuk angin", 26);
+        tua.set("panas dalam", 13);
+        tua.set("kejang", 22);
+        tua.set("katarak", 31);
+        tua.set("panu", 13);
+        
+        barModel.addSeries(bayi);
+        barModel.addSeries(balita);
+        barModel.addSeries(anak);
+        barModel.addSeries(rema);
+        barModel.addSeries(dws);
+        barModel.addSeries(tua);
+        
+        
+        Axis xAxis = barModel.getAxis(AxisType.X);
+        xAxis.setLabel("Penyakit");
+        
+        Axis yAxis = barModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Usia");
+        yAxis.setMin(0);
+        yAxis.setMax(500);
     }
      
   
